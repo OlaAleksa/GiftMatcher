7323c80b1db46d96d5984a8a84d3d5de14b442c3
Aleksandra Gaul <aleksandragaul@MacBook-Air-Aleksandra.local>
2023-09-21 19:49:46 +0200

    init: project installation

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,3 @@
+<template>
+    Jestem 
+</template>
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import vue from "@vitejs/plugin-vue";
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        vue({
+            template: {
+                transformAssetUrls: {
+                    includeAbsolute: false,
+                },
+            },
+        }),
+        laravel({
+            input: ['resources/css/app.scss', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+});
